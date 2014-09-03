31ee7798c1ca54a31a4678693c748f863c324860
Kyle Buzby <kyle.buzby@gmail.com>
2014-09-03 08:12:40 -0400

    VOID problem works in theory but does not work for a large grid (over ~15), the new euler15 uses pascals triangle to solve mathmatically for the answer

--- euler15.py
+++ euler15.py
@@ -1,32 +1,12 @@
-n = 20
+def factorial(n):
+	factorialValue=1
+	while n > 1:
+		factorialValue *=n
+		n -= 1
+	return factorialValue
 
-def find_all_paths(graph,start,end,path=[]):
-	path = path + [start]
-	if start == end:
-		return [path]
-	if not graph.has_key(start):
-		return []
-	paths = []
-	for node in graph[start]:
-		if node not in path:
-			newpaths = find_all_paths(graph,node,end,path)
-			for newpath in newpaths:
-				paths.append(newpath)
-	return paths
+def number_combos(n, k):
+	numberOfCombos = factorial(n) / (factorial(k) * factorial(n - k))
+	return numberOfCombos
 
-
-graph = {}
-for x in range(0,n+1):
-	for y in range(0,n+1):
-		if (x !=n and y!=n):
-			graph[str(x).zfill(2)+str(y).zfill(2)] = [str(x+1).zfill(2)+str(y).zfill(2), str(x).zfill(2)+str(y+1).zfill(2)]
-		elif (x==n):
-			graph[str(x).zfill(2)+str(y).zfill(2)] = [str(x).zfill(2)+str(y+1).zfill(2)]
-		elif (y==n):
-			graph[str(x).zfill(2)+str(y).zfill(2)] = [str(x+1).zfill(2)+str(y).zfill(2)]
-		elif (x==n and y==n):
-			continue
-		
-
-result = find_all_paths(graph, '0000', str(n).zfill(2)+str(n).zfill(2))	
-print len(result)
+print (number_combos(40, 20))
